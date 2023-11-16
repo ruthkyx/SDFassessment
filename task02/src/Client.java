88f771d598b606da01442cc75f95551576557338
your_name <ruth.kyx@gmail.com>
2023-11-16 15:36:32 +0800

    update

--- task02/src/Client.java
+++ task02/src/Client.java
@@ -1,3 +1,50 @@
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
-    
+    public static void main(String[] args) throws UnknownHostException, IOException {
+
+        switch (args.length) {
+            case (args[]):
+
+            case (args[1]):
+
+            case (args[2]):
+            
+        
+        }
+
+            while (true) {
+                Socket socket = server.accept();
+                System.out.println("connection established");
+                Socket socket = new Socket();      
+            }
+        
+
+        try (InputStream is = socket.getInputStream()) {
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader (isr);
+
+            Buffer dis = new DataInputStream(bis);
+            String line = dis.readUTF();
+
+            String reply = br.readLine();
+            System.out.println(reply);
+
+            Scanner scanner = new Scanner(System.in);
+            String userInput;
+            while (!"close".equals(userInput = scanner.nextLine())) {
+                
+                bw.write(userInput+"\n");
+                bw.flush();
+
+        } catch (EOFException e) {
+            socket.close();
+        }
+    }
+
 }
--- task02/src/Server.java
+++ task02/src/Server.java
@@ -1,3 +1,43 @@
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+
 public class Server {
     
+    public static final Integer DEFAULT_PORT = 3000;
+    public static void main (String[] args) throws Exception {
+
+        int port = DEFAULT_PORT; 
+        String serverProg = "";
+        switch (args.length) {
+            case 1:
+            serverProg = args[0];
+            break;
+
+            case 2: 
+            serverProg = args[1];
+            port = Integer.parseInt(args[1]);
+            break;
+
+            case 3; 
+            serverProg = args[2];
+            port = Integer.parseInt(args[2]);
+
+            default: 
+                System.err.println("Error!");
+                System.exit(1);
+        }
+
+        ServerSocket server = new ServerSocket(port);
+
+        try (OutputStream os = ServerSocket.getOutputStream()) {
+            OutputStreamWriter osw = new OutputStreamWriter (os);
+            BufferedWriter bw = new BufferedWriter(osw);
+            
+            bw.flush();
+        }
+    
+
+    }
 }
